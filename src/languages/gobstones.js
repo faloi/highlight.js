4ff975a868890538b852f5955870543d3494b267
Federico Aloi <federico.aloi@gmail.com>
2015-05-05 23:21:48 -0300

    Support for colors and directions

--- src/languages/gobstones.js
+++ src/languages/gobstones.js
@@ -6,7 +6,10 @@ Description: A programming language designed for teaching the fundamental abstra
 
 function(hljs) {
   return {
-    keywords: 'program procedure function',
+    keywords: {
+      keyword: 'program procedure function',
+      literal: 'Verde Azul Rojo Negro Norte Este Sur Oeste',
+    },
     contains: [ hljs.NUMBER_MODE, hljs.C_LINE_COMMENT_MODE, hljs.C_BLOCK_COMMENT_MODE, hljs.HASH_COMMENT_MODE ]
   }
 }
