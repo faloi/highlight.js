5cef4c453a10c0b7ebfe3179412390b9a6c0c9ce
Federico Aloi <federico.aloi@gmail.com>
2015-05-05 23:35:18 -0300

    Added keywords, built in funcs and procs and operators

--- src/languages/gobstones.js
+++ src/languages/gobstones.js
@@ -7,8 +7,10 @@ Description: A programming language designed for teaching the fundamental abstra
 function(hljs) {
   return {
     keywords: {
-      keyword: 'program procedure function',
-      literal: 'Verde Azul Rojo Negro Norte Este Sur Oeste',
+      keyword: 'program procedure function return if then else switch repeat while foreach in',
+      literal: 'Verde Azul Rojo Negro Norte Este Sur Oeste False True',
+      built_in: 'Poner Sacar Mover IrAlBorde VaciarTablero',
+      operator: 'not div mod siguiente previo opuesto hayBolitas nroBolitas puedeMover minBool maxBool minDir maxDir minColor maxColor'
     },
     contains: [ hljs.NUMBER_MODE, hljs.C_LINE_COMMENT_MODE, hljs.C_BLOCK_COMMENT_MODE, hljs.HASH_COMMENT_MODE ]
   }
