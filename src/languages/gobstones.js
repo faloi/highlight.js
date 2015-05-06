dc06a38f84b6fc2094708259c41a18114ae282da
Federico Aloi <federico.aloi@gmail.com>
2015-05-05 23:17:05 -0300

    Created gobstones definition with a few keywords

---
+++ src/languages/gobstones.js
@@ -0,0 +1,11 @@
+/*
+Language: Gobstones
+Author: Federico Aloi <federico.aloi@gmail.com>
+Description: A programming language designed for teaching the fundamental abstractions for algorithmic problem solving
+*/
+
+function(hljs) {
+  return {
+    keywords: 'program procedure function'
+  }
+}
